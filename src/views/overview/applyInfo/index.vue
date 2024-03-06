29777eeb2993f243a89a2197f1a3d426795943f2
thekidgithub <631046343@qq.com>
2024-03-06 17:00:22 +0800

    完成仪表盘页主体部分

--- src/views/overview/applyInfo/index.vue
+++ src/views/overview/applyInfo/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <div> 11111 </div>
+  <div class="flex">
+    <div class="flex flex-col w-2/3">
+      <overview></overview>
+      <stages></stages>
+    </div>
+    <div class="w-1/3 bg-white ml-5">
+      <schedules></schedules>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
-
-<script></script>
+<script setup lang="ts">
+import overview from './components/overview.vue';
+import stages from './components/stages.vue';
+import schedules from './components/schedules.vue';
+</script>
 
 <style scoped lang="less"></style>
