431c15c8d55c3720fa2069ab903ec66809e91e11
Eason_hk <3108573607@qq.com>
2024-03-30 11:57:59 +0800

    calendar

--- src/views/interview/schedule/index.vue
+++ src/views/interview/schedule/index.vue
@@ -1,7 +1,30 @@
 <template>
-  <div> 333 </div>
+  <div>
+    <div class="bg-white w-full h-full p-5">
+      <a-calendar :model-value="startDate" @update:modelValue="updateValue" />
+      <p>Selected Date: {{ formattedDate }}</p>
+    </div>
+    <div> </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
-
-<style scoped lang="less"></style>
+<script setup>
+import { ref } from 'vue';
+// 格式化日期
+const formatDate = (date) => {
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    weekday: 'long',
+  };
+  return date.toLocaleDateString('en-US', options);
+};
+const startDate = ref(new Date('2024-01-01'));
+const formattedDate = ref(formatDate(startDate.value));
+// 更新日期
+const updateValue = (newValue) => {
+  startDate.value = newValue;
+  formattedDate.value = formatDate(newValue);
+};
+</script>
