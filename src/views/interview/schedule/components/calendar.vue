9a4f0e4f8a53c8e21ade05d776f62f6c0913aad2
Eason_hk <3108573607@qq.com>
2024-04-06 17:05:03 +0800

    update schedules logic

--- src/views/interview/schedule/components/calendar.vue
+++ src/views/interview/schedule/components/calendar.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="flex-4 bg-white rounded-sm">
-    <a-calendar :model-value="startDate" @change="handleClick" />
+    <a-calendar :model-value="curDate" @change="handleClick" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const startDate = ref(new Date('2024-01-01'));
+const curDate = ref(new Date('2024-01-01'));
 const emits = defineEmits(['dateClick']);
 
-// 日历组件的点击事件处理函数
+// 点击某天的事件处理函数
 const handleClick = (date: Date) => {
+  curDate.value = date;
   emits('dateClick', date); // 触发自定义事件，传递选中日期
 };
 </script>
