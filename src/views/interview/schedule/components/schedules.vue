7081a9f37aa5450c8f0040ad3f235e25636ba18f
Eason_hk <3108573607@qq.com>
2024-04-21 01:04:36 +0800

    calendar component default slot

--- src/views/interview/schedule/components/schedules.vue
+++ src/views/interview/schedule/components/schedules.vue
@@ -54,6 +54,42 @@
           </div>
         </li>
       </div>
+      <!-- <div class="flex flex-col">
+        <li
+          v-for="info in schedulesEg"
+          :key="info.name"
+          class="list-none mb-2.5"
+        >
+          <div class="overflow-hidden flex items-center mb-1">
+            <icon-calendar
+              class="w-9 h-9 text-xl rounded-full border-2 p-2 bg-gray-100 text-blue-600 float-left mr-2.5"
+            />
+            <span class="float-left"
+              >{{ info.name }}</span
+            >
+          </div>
+          <div class="overflow-hidden flex items-center">
+            <icon-clock-circle
+              class="w-9 h-9 text-xl p-2 float-left mr-2.5 text-gray-500"
+            />
+            <span class="float-left leading-10 text-slate-500">{{
+              info.time
+            }}</span>
+          </div>
+
+          <div class="overflow-hidden flex items-center">
+            <icon-user
+              class="w-9 h-9 text-xl p-2 float-left mr-2.5 text-gray-500"
+            />
+            <span class="float-left leading-10 text-slate-500">{{
+              info.player
+            }}</span>
+          </div>
+          <span class="float-left pl-2 leading-10 text-slate-400 text-sm">{{
+            info.description
+          }}</span>
+        </li>
+      </div> -->
     </a-scrollbar>
 
     <a-button
@@ -126,4 +162,54 @@ const interviewType = (info: CandidateInfo) => {
   const teamInterview = 'common.steps.TeamInterview';
   return t(info.step === 'GroupInterview' ? groupInterview : teamInterview);
 };
+
+// const schedulesEg = [
+//   {
+//     date: new Date('2024-01-01'),
+//     name: 'PM组面',
+//     time: '13.00-14.00（1h）',
+//     player: '苦瓜',
+//     description: '可以调时间',
+//   },
+//   {
+//     date: new Date('2024-01-01'),
+//     name: 'Design组面',
+//     time: '19:00-21:00（2h）',
+//     location: '810',
+//     player: '周子涵',
+//     description: '可以调时间',
+//   },
+//   {
+//     date: new Date('2024-01-01'),
+//     name: 'web组面',
+//     time: '13:00-13:30（0.5h）',
+//     location: '811',
+//     player: '柴犬',
+//     description: '没别的时间了',
+//   },
+//   {
+//     date: new Date('2024-01-02'),
+//     name: 'web组面',
+//     time: '13:00-13:30（0.5h）',
+//     location: '811',
+//     player: 'kid',
+//     description: '没别的时间了',
+//   },
+//   {
+//     date: new Date('2024-01-02'),
+//     name: 'web组面',
+//     time: '13:00-13:30（0.5h）',
+//     location: '811',
+//     player: '一架飞机',
+//     description: '可以调时间',
+//   },
+//   {
+//     date: new Date('2024-01-03'),
+//     name: 'web组面',
+//     time: '13:00-13:30（0.5h）',
+//     location: '811',
+//     player: '菜菜子',
+//     description: '没别的时间了',
+//   },
+// ]; // 测试
 </script>
