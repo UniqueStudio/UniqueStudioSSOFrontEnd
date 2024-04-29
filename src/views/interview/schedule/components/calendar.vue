e314b399790ef8a8289f22dc43cdc40db4c44277
Eason_hk <3108573607@qq.com>
2024-04-29 19:25:29 +0800

    complete calendar default slots (version 2.0)

--- src/views/interview/schedule/components/calendar.vue
+++ src/views/interview/schedule/components/calendar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex-4 bg-white rounded-sm h-full">
-    <a-calendar v-model="curDate">
+    <a-calendar v-model="curDate" class="month-row">
       <template #default="{ year, month, date }">
         <div
           class="arco-calendar-date"
@@ -9,28 +9,49 @@
           <div class="arco-calendar-date-value">
             <div class="arco-calendar-date-circle">{{ date }}</div>
           </div>
-          <div v-for="(info, index) in props.infos" :key="info.step">
-            <div
-              v-if="
-                formatToday(info.date) === formatDate(year, month, date) &&
-                index < 2
-              "
-              class="flex intems-center mb-1"
-            >
+          <div
+            v-for="(info, index) in filteredInfos(year, month, date).slice(
+              0,
+              2,
+            )"
+            :key="formatToday(info.date)"
+          >
+            <div v-if="index < 2" class="flex items-center mb-1">
               <a-badge
                 class="float-left mr-2 flex items-center justify-center"
-                :color="COLORS[index]"
+                :color="COLORS[index % COLORS.length]"
               ></a-badge>
-              <span class="float-left"
+              <span class="float-left text-sm"
                 >{{ info.group }}{{ interviewType(info) }}</span
               >
             </div>
-            <div v-else-if="index >= 2">
-              <span class="float-left text-blue-500">{{
-                `剩余${props.infos.length - 2}条信息`
-              }}</span>
+          </div>
+          <div v-if="hasMoreThanTwoInfos(year, month, date)">
+            <span class="float-left text-blue-600 text-sm">{{
+              `剩余${filteredInfos(year, month, date).length - 2}个日程`
+            }}</span>
+          </div>
+          <!-- <div v-for="(info, index) in filteredInfos(year, month, date).slice(0,2)" :key="info.date">
+            <div
+              v-if="index < 2"
+              class="flex items-center mb-1"
+            >
+              <a-badge
+                class="float-left mr-2 flex items-center justify-center"
+                :color="COLORS[index % COLORS.length]"
+              ></a-badge>
+              <span class="float-left text-sm"
+                >{{ info.name }}</span
+              >
             </div>
           </div>
+          <div
+            v-if="hasMoreThanTwoInfos(year, month, date)"
+          >
+            <span class="float-left text-blue-600 text-sm">{{
+              `剩余${filteredInfos(year,month,date).length - 2}个日程`
+            }}</span>
+          </div> -->
         </div>
       </template>
     </a-calendar>
@@ -38,7 +59,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, inject } from 'vue';
+import { computed, ref, inject } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 interface InterviewInfo {
@@ -74,6 +95,120 @@ const interviewType = (info: InterviewInfo) => {
   const teamInterview = 'common.steps.TeamInterview';
   return t(info.step === 'GroupInterview' ? groupInterview : teamInterview);
 };
+
+const filteredInfos = (year: number, month: number, date: number) => {
+  return props.infos.filter(
+    (info) => formatToday(info.date) === formatDate(year, month, date),
+  );
+};
+
+const hasMoreThanTwoInfos = (
+  year: number,
+  month: number,
+  date: number,
+): boolean => {
+  const limitedInfos = filteredInfos(year, month, date).slice(0, 2);
+  return (
+    props.infos.filter(
+      (info) => formatToday(info.date) === formatDate(year, month, date),
+    ).length > limitedInfos.length
+  );
+};
+// const schedulesEg = [
+//   {
+//     date: '2024-01-01',
+//     name: 'PM组面',
+//     time: '13.00-14.00（1h）',
+//     player: '苦瓜',
+//     description: '可以调时间',
+//   },
+//   {
+//     date: '2024-01-01',
+//     name: 'Design组面',
+//     time: '19:00-21:00（2h）',
+//     location: '810',
+//     player: '周子涵',
+//     description: '可以调时间',
+//   },
+//   {
+//     date: '2024-01-01',
+//     name: 'web组面',
+//     time: '13:00-13:30（0.5h）',
+//     location: '811',
+//     player: '柴犬',
+//     description: '没别的时间了',
+//   },
+//   {
+//     date: '2024-01-02',
+//     name: 'web组面',
+//     time: '13:00-13:30（0.5h）',
+//     location: '811',
+//     player: 'kid',
+//     description: '没别的时间了',
+//   },
+//   {
+//     date: '2024-01-02',
+//     name: 'web组面',
+//     time: '13:00-13:30（0.5h）',
+//     location: '811',
+//     player: '一架飞机',
+//     description: '可以调时间',
+//   },
+//   {
+//     date: '2024-01-03',
+//     name: 'web组面',
+//     time: '13:00-13:30（0.5h）',
+//     location: '811',
+//     player: '菜菜子',
+//     description: '没别的时间了',
+//   },
+//   {
+//     date: '2024-01-03',
+//     name: 'web组面',
+//     time: '13:00-13:30（0.5h）',
+//     location: '811',
+//     player: '菜菜子',
+//     description: '没别的时间了',
+//   },
+//   {
+//     date: '2024-01-03',
+//     name: 'web组面',
+//     time: '13:00-13:30（0.5h）',
+//     location: '811',
+//     player: '菜菜子',
+//     description: '没别的时间了',
+//   },
+//   {
+//     date: '2024-01-03',
+//     name: 'web组面',
+//     time: '13:00-13:30（0.5h）',
+//     location: '811',
+//     player: '菜菜子',
+//     description: '没别的时间了',
+//   },
+//   {
+//     date: '2024-01-03',
+//     name: 'web组面',
+//     time: '13:00-13:30（0.5h）',
+//     location: '811',
+//     player: '菜菜子',
+//     description: '没别的时间了',
+//   },
+//   {
+//     date: '2024-01-03',
+//     name: 'web组面',
+//     time: '13:00-13:30（0.5h）',
+//     location: '811',
+//     player: '菜菜子',
+//     description: '没别的时间了',
+//   },
+// ]; // 测试
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.month-row {
+  :deep(.arco-calendar-month-row) {
+    height: 115px;
+  }
+}
+</style>
