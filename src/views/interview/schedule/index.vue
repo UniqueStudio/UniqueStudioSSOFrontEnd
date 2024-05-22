1c4a5257605ae4a6f541aa021a124d6c176cb065
Eason <3108573607@qq.com>
2024-05-22 16:13:40 +0800

    2.0

--- src/views/interview/schedule/index.vue
+++ src/views/interview/schedule/index.vue
@@ -46,14 +46,15 @@ function formatTime(date: Date): string {
   });
 }
 
-function calculateDuration(start: Date, end: Date): string {
+function Duration(start: Date, end: Date): string {
+  const timeRange = `${formatTime(start)}-${formatTime(end)}`;
   const diffInMs = end.getTime() - start.getTime();
   const diffInMinutes = Math.round(diffInMs / 1000 / 60);
   const hours = Math.floor(diffInMinutes / 60);
   const minutes = diffInMinutes % 60;
-  if (!hours) return `${minutes}min`;
-  if (!minutes) return `${hours}h`;
-  return `${hours}h${minutes}min`;
+  if (!hours) return `${timeRange}(${minutes}min)`;
+  if (!minutes) return `${timeRange}(${hours}h)`;
+  return `${timeRange}(${hours}h${minutes}min)`;
 }
 
 const props = defineProps({
