83613e5ee276fdfb6776e28a863ca046f0f93328
JerryC66 <13545209616@163.com>
2024-06-27 00:28:28 +0800

    fix: 修正状态编码

--- src/locale/zh-CN/common.ts
+++ src/locale/zh-CN/common.ts
@@ -114,9 +114,9 @@ export default {
   'common.yes': '是',
   'common.no': '否',
   'common.void': '无',
-  'common.rec.name.S': '夏令营',
-  'common.rec.name.C': '秋季招新',
-  'common.rec.name.A': '春季招新',
+  'common.rec.name.S': '春季招新',
+  'common.rec.name.C': '夏令营',
+  'common.rec.name.A': '秋季招新',
 
   'common.applyInfo.overview': '数据总览',
   'common.applyInfo.groupMemberCounts': '团队人数',
